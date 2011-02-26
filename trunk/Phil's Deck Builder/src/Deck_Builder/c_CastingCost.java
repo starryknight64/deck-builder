7c0bbf228d05d7e4a2a1bf720d69bb568308dcec
pdponzer@gmail.com <pdponzer@gmail.com@5b5714d9-8dd1-44dc-c5ef-05513a7c762f>
2011-02-26 18:27:00 +0000

    Fixed issue with "Little Girl" Un- card

--- trunk/Phil's Deck Builder/src/Deck_Builder/c_CastingCost.java
+++ trunk/Phil's Deck Builder/src/Deck_Builder/c_CastingCost.java
@@ -70,23 +70,9 @@ public class c_CastingCost extends c_Colors {
 
     public Integer getCMC() {
         Integer cmc = 0;
-//        int j;
-//        boolean hasDigits;
 
         for (int i = 0; i < m_glyphs.size(); i++) {
             cmc += getGlyphCMC( m_glyphs.get( i ) );
-//            j = 0;
-//            hasDigits = false;
-//            while( Character.isDigit( m_glyphs.get(i).charAt(j) ) ) {
-//                hasDigits = true;
-//                j++;
-//            }
-//
-//            if (hasDigits) {
-//                cmc += Integer.parseInt( m_glyphs.get(i).substring(0, j) );
-//            } else if( !m_glyphs.get(i).equals( "X" ) ) {
-//                cmc++;
-//            }
         }
         return cmc;
     }
@@ -136,7 +122,7 @@ public class c_CastingCost extends c_Colors {
         }
 
         ImageIcon glyph;
-        int width_in = ( m_glyphs.get( 0 ).equals( "1000000" ) ? 61 : GLYPH_WIDTH_IN );
+        int width_in = getWidthIn( m_glyphs.get( 0 ) );
         BufferedImage img = new BufferedImage( width_in * m_glyphs.size(), GLYPH_HEIGHT_IN, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics2D g = img.createGraphics();
         String desc = "";
@@ -150,9 +136,30 @@ public class c_CastingCost extends c_Colors {
         glyph = null;
         ////System.gc();
 
-        int width_out = ( m_glyphs.get( 0 ).equals( "1000000" ) ? 39 : GLYPH_WIDTH_OUT );
+        int width_out = getWidthOut( m_glyphs.get( 0 ) );
         ImageIcon icon = new ImageIcon( img.getScaledInstance( width_out * m_glyphs.size(), GLYPH_HEIGHT_OUT, Image.SCALE_SMOOTH ), "" );
         icon.setDescription( desc.substring( 0, desc.length()-1 ) );
         return icon;
+    }
+
+    private int getWidthIn( String glyph ) {
+        int width_in = GLYPH_WIDTH_IN;
+        if( glyph.equals( "1000000" ) ) {
+            width_in = 61; // Gleemax
+        } else if( glyph.equals( "500" ) ) {
+            width_in = 13; // Little Girl
+        }
+
+        return width_in;
+    }
+    private int getWidthOut( String glyph ) {
+        int width_out = GLYPH_WIDTH_OUT;
+        if( glyph.equals( "1000000" ) ) {
+            width_out = 39; // Gleemax
+        } else if( glyph.equals( "500" ) ) {
+            width_out = 8; // Little Girl
+        }
+
+        return width_out;
     }
 }
