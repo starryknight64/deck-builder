74c9d5dd39109de54a93e9ebb378dad5a34b23d6
pdponzer@gmail.com <pdponzer@gmail.com@5b5714d9-8dd1-44dc-c5ef-05513a7c762f>
2011-03-04 18:26:57 +0000

    Removed unused code

--- Phil's Deck Builder/src/Deck_Builder/Main.java
+++ Phil's Deck Builder/src/Deck_Builder/Main.java
@@ -18,30 +18,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    String s = "hello|\nworld*foo*bar\r\nEUREKA!";
-    System.out.println(s);
-    // prints:
-    // hello|world*foo*bar
-    // EUREKA!
-
-    Scanner sc;
-
-    sc = new Scanner(s);
-    sc.useDelimiter("\r\n");
-    while (sc.hasNext()) {
-        System.out.print("[" + sc.next() + "]");
-    }
-    // prints:
-    // [hello][world][foo][bar
-    // EUREKA!]
-
-    System.out.println();
-
-    sc = new Scanner(s).useDelimiter("\r?\n|\r|\\|");
-    while (sc.hasNext()) {
-        System.out.print("[" + sc.next() + "]");
-    }
-
         try {
             String lookAndFeel = UIManager.getSystemLookAndFeelClassName();
             if( lookAndFeel != null ) {
@@ -54,9 +30,6 @@ public class Main {
         MainWindow w = new MainWindow();
         w.setVisible( true );
         w = null;
-        //System.gc();
-//        MainFrame frame = new MainFrame();
-//        frame.setVisible(true);
     }
 
 }
